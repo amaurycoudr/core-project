17d1ec74f7656ebb3327f6b6fe5d11fdd526cc4c
amaurycoudr <amaury.coudray@gmail.com>
2025-03-15 09:13:23 +0100

    chore(front): fix expo dev client

--- apps/front/metro.config.js
+++ apps/front/metro.config.js
@@ -1,20 +1,45 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+
+const path = require("path");
+const { FileStore } = require("metro-cache");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
 const { getDefaultConfig } = require("expo/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
-const path = require("node:path");
+const projectDir = __dirname;
+const workspaceRoot = path.resolve(projectDir, "../..");
 
-const projectRoot = __dirname;
-
-const monorepoRoot = path.resolve(projectRoot, "../..");
+const symlinksResolver = MetroSymlinksResolver();
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const expoConfig = getDefaultConfig(projectDir);
 
-config.watchFolders = [monorepoRoot];
+/** @type {import('expo/metro-config').MetroConfig} */
+module.exports = makeMetroConfig({
+  ...expoConfig,
+  resolver: {
+    ...expoConfig.resolver,
+    resolveRequest: (context, moduleName, platform) => {
+      try {
+        // Symlinks resolver throws when it can't find what we're looking for.
+        const res = symlinksResolver(context, moduleName, platform);
 
-config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, "node_modules"),
-	path.resolve(monorepoRoot, "node_modules"),
-];
-
-module.exports = config;
+        if (res) {
+          return res;
+        }
+      } catch {
+        // If we have an error, we pass it on to the next resolver in the chain,
+        // which should be one of expos.
+        // https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
+        return context.resolveRequest(context, moduleName, platform);
+      }
+    },
+  },
+  watchFolders: [
+    workspaceRoot
+  ],
+  cacheStores: [
+    new FileStore({
+      root: path.join(projectDir, "node_modules", ".cache", "metro"),
+    }),
+  ]
+});
